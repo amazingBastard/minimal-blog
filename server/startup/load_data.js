6f311d1de6704439faf902bfb519d5e8e2be47f3
Damir <damir@withpulp.com>
2015-04-07 22:03:50 -0400

    added projects fixtures, added hover states to admin buttons

--- server/startup/load_data.js
+++ server/startup/load_data.js
@@ -13,11 +13,27 @@ Meteor.startup(function () {
     loadUser(users[key]);
   }
 
+  var now = Date.now(),
+      admin = Meteor.users.findOne();
+
   if (Posts.find().count() === 0) {
-    var now = Date.now(),
-        admin = Meteor.users.findOne();
 
     Posts.insert({
+      title: 'Hello World',
+      intro: 'This is a software blog.',
+      content: '##Let us begin!\n\nThis is only a test to verify that your app is working.',
+      created: now,
+      updated: now,
+      author: admin.username,
+      contentCall: function() {
+        return this.content;
+      }
+    });
+  }
+
+  if (Projects.find().count() === 0) {
+
+    Projects.insert({
       title: 'Hello World',
       intro: 'This is a software blog.',
       content: '##Let us begin!\n\nThis is only a test to verify that your app is working.',
